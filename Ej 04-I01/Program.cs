ef13055565e29b5432c31f6b491b7424c0303d2e
GuidoInsua <insua.guido@gmail.com>
2023-04-10 19:25:56 -0300

    Ej 1 de sobrecarga terminado

---
+++ Ej 04-I01/Program.cs
@@ -0,0 +1,35 @@
+﻿//Guido Insua
+
+namespace Ej_04_I01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio I01 - Sumador";
+            
+            Sumador unSumador = new Sumador();
+
+            Console.WriteLine(unSumador.Sumar(3, 5));
+            Console.WriteLine(unSumador.Sumar("Hola", ", como estas?"));
+
+            int cantidad = (int)unSumador;
+
+            Console.WriteLine(cantidad);
+
+            Sumador otroSumador = new Sumador(2);
+
+            if (unSumador | otroSumador)
+            {
+                Console.WriteLine("Iguales");
+            }
+            else
+            {
+                Console.WriteLine("Distintos");
+            }
+
+            Console.WriteLine(unSumador + otroSumador);
+
+        }
+    }
+}
---
+++ Ej 04-I01/Sumador.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_04_I01
+{
+    internal class Sumador
+    {
+        private int _cantidadSumas;
+
+        public Sumador(int cantidadSumas)
+        {
+            _cantidadSumas = cantidadSumas;
+        }
+
+        public Sumador():this(0) { }
+
+        public long Sumar(long a, long b) 
+        {
+            _cantidadSumas++;
+            return a + b;
+        }
+
+        public string Sumar(string a, string b) 
+        {
+            _cantidadSumas++;
+            return a + b;
+        }
+
+        public static explicit operator int(Sumador v)
+        {
+            return v._cantidadSumas;
+        }
+
+        public static bool operator |(Sumador v1, Sumador v2)
+        {
+            if ((int)v1 == (int)v2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static long operator +(Sumador v1, Sumador v2)
+        {
+            return (int)v1 + (int)v2;
+        }
+    }
+}
